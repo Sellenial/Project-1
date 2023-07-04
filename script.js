4eb622a4fc20f793e666c82201cbdf5686181d9e
Sellenial <138551710+Sellenial@users.noreply.github.com>
2023-07-04 11:29:12 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// Get references to the buttons and the displayed image element
+const button1 = document.getElementById('button1');
+const button2 = document.getElementById('button2');
+const button3 = document.getElementById('button3');
+const displayedImage = document.getElementById('displayedImage');
+
+// Set event listeners for each button
+button1.addEventListener('click', () => {
+  displayedImage.src = 'https://media.discordapp.net/attachments/1037900757585907772/1042018372793139360/starwatchernilla.png';
+});
+
+button2.addEventListener('click', () => {
+  displayedImage.src = 'https://cdn.discordapp.com/attachments/854544686789165076/1125309497527779429/f45726b8e93bd5772146b8ebedea56e0.png';
+});
+
+button3.addEventListener('click', () => {
+  displayedImage.src = 'https://cdn.discordapp.com/attachments/555738743953162240/1124914217456250963/176.jpg';
+});
